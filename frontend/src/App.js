b30c79d577eb963034b7cdedee657277d64b128b
prateek-bka <prateek.bka@gmail.com>
2022-11-22 14:52:43 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/layout/Header";
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
+import "./styles/founder.scss";
 
 function App() {
   return (
